d01f7c3fce6cc80a3fb21c9fb9d1cd81bc4b15b3
Elisha Lai <elisha.lai@outlook.com>
2016-12-03 23:41:23 -0500

    minor changes

--- packet.java
+++ packet.java
@@ -14,63 +14,63 @@ import java.net.DatagramPacket;
 import java.nio.ByteBuffer;
 
 public class packet {
-	public static final int MAX_DATA_LENGTH = 500;
-	public static final int SEQ_NUM_MODULO = 32;
-   
+   public static final int MAX_DATA_LENGTH = 500;
+   public static final int SEQ_NUM_MODULO = 32;
+
    private static final int ACK_PACKET_TYPE = 0;
    private static final int DATA_PACKET_TYPE = 1;
    private static final int EOT_PACKET_TYPE = 2;
 	
-	private int type;
-	private int seqNum;
-	private String data;
+   private int type;
+   private int seqNum;
+   private String data;
 
-	// Hidden constructor to prevent creation of invalid packets
-	private packet(int type, int seqNum, String data) throws Exception {
-		// Throws an exception if data seqment larger than allowed
-		if (data.length() > MAX_DATA_LENGTH) {
-		   System.out.println("ERROR: Expecting data field which is at most" +
-		   	  " 500 characters, but got " + data.length() + " characters");
+   // Hidden constructor to prevent creation of invalid packets
+   private packet(int type, int seqNum, String data) throws Exception {
+      // Throws an exception if data seqment larger than allowed
+      if (data.length() > MAX_DATA_LENGTH) {
+         System.out.println("ERROR: Expecting data field which is at most" +
+            " 500 characters, but got " + data.length() + " characters");
          System.exit(-1);
-		} // if
-			
-		this.type = type;
-		this.seqNum = seqNum % SEQ_NUM_MODULO;
-		this.data = data;
-	} // Constructor
-	
-	// Special packet constructors to be used in place of hidden constructor
-	public static packet createACK(int seqNum) throws Exception {
-		return new packet(ACK_PACKET_TYPE, seqNum, new String());
-	} // Constructor
-	
-	public static packet createData(int seqNum, String data) throws Exception {
-		return new packet(DATA_PACKET_TYPE, seqNum, data);
-	} // Constructor
-	
-	public static packet createEOT(int seqNum) throws Exception {
-		return new packet(EOT_PACKET_TYPE, seqNum, new String());
-	} // Constructor
-	
-	// Returns the type
-	public int getType() {
-		return type;
-	} // getType
-	
-	// Returns the sequence number
-	public int getSeqNum() {
-		return seqNum;
-	} // getSeqNum
-	
-	// Returns the length of the data
-	public int getLength() {
-		return data.length();
-	} // getLength
-	
-	// Returns the data as a byte array representation
-	public byte[] getData() {
-		return data.getBytes();
-	} // getData
+      } // if
+		
+      this.type = type;
+      this.seqNum = seqNum % SEQ_NUM_MODULO;
+      this.data = data;
+   } // Constructor
+
+   // Special packet constructors to be used in place of hidden constructor
+   public static packet createACK(int seqNum) throws Exception {
+      return new packet(ACK_PACKET_TYPE, seqNum, new String());
+   } // Constructor
+
+   public static packet createData(int seqNum, String data) throws Exception {
+      return new packet(DATA_PACKET_TYPE, seqNum, data);
+   } // Constructor
+
+   public static packet createEOT(int seqNum) throws Exception {
+      return new packet(EOT_PACKET_TYPE, seqNum, new String());
+   } // Constructor
+
+   // Returns the type
+   public int getType() {
+      return type;
+   } // getType
+
+   // Returns the sequence number
+   public int getSeqNum() {
+      return seqNum;
+   } // getSeqNum
+
+   // Returns the length of the data
+   public int getLength() {
+      return data.length();
+   } // getLength
+
+   // Returns the data as a byte array representation
+   public byte[] getData() {
+      return data.getBytes();
+   } // getData
 
    // Checks if a packet is an ACK packet
    public boolean isACK() {
@@ -101,26 +101,26 @@ public class packet {
 	
    // Creates a packet to send data to the emulator and writes it out to the
    // data transfer socket
-	public void sendTo(String emulatorAddress, int emulatorPort,
-		DatagramSocket dataTransferSocket) throws Exception {
+   public void sendTo(String emulatorAddress, int emulatorPort,
+      DatagramSocket dataTransferSocket) throws Exception {
       byte[] dataToEmulator = getDataToEmulator();
       InetAddress emulatorIPAddress = InetAddress.getByName(emulatorAddress);
       DatagramPacket packetToEmulator =
          new DatagramPacket(dataToEmulator, dataToEmulator.length,
             emulatorIPAddress, emulatorPort);
       dataTransferSocket.send(packetToEmulator);
-	} // sendTo
+   } // sendTo
 	
    // Gets data to be sent to the emulator
-	private byte[] getDataToEmulator() throws Exception {
-		ByteBuffer buffer = ByteBuffer.allocate(512);
-		buffer.putInt(type);
+   private byte[] getDataToEmulator() throws Exception {
+      ByteBuffer buffer = ByteBuffer.allocate(512);
+      buffer.putInt(type);
       buffer.putInt(seqNum);
       buffer.putInt(data.length());
       buffer.put(data.getBytes(), 0, data.length());
-		return buffer.array();
-	} // getDataToEmulator
-	
+      return buffer.array();
+   } // getDataToEmulator
+
    // Creates a packet to receive data from the emulator and reads into it 
    // from the data transfer socket
    public static packet receiveFrom(DatagramSocket dataTransferSocket)
@@ -134,14 +134,14 @@ public class packet {
    } // receiveFrom
 
    // Parses data received from the emulator
-	private static packet parseDataFromEmulator(byte[] dataFromEmulator)
+   private static packet parseDataFromEmulator(byte[] dataFromEmulator)
       throws Exception {
-		ByteBuffer buffer = ByteBuffer.wrap(dataFromEmulator);
-		int type = buffer.getInt();
-		int seqNum = buffer.getInt();
-		int length = buffer.getInt();
-		byte[] data = new byte[length];
-		buffer.get(data, 0, length);
-		return new packet(type, seqNum, new String(data));
-	} // parseDataFromEmulator
+      ByteBuffer buffer = ByteBuffer.wrap(dataFromEmulator);
+      int type = buffer.getInt();
+      int seqNum = buffer.getInt();
+      int length = buffer.getInt();
+      byte[] data = new byte[length];
+      buffer.get(data, 0, length);
+      return new packet(type, seqNum, new String(data));
+   } // parseDataFromEmulator
 }
